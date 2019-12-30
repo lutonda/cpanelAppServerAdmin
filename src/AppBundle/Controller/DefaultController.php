9cc5b5816ea1557a8be3babc9e784deafe28ed10
Sebastiõ Dias Lutonda <lutonda@gmail.com>
2019-12-30 12:12:32 +0000

    dashboard build

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,7 +6,7 @@ use AppBundle\Application\Application as App;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Symfony\Component\Yaml\Yaml;
 class DefaultController extends Controller
 {
     /**
@@ -14,11 +14,17 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+
+        
+        $source_app=__DIR__.'/../../..';//$this->getParameter('paths')['source_app'];
+        $file=Yaml::parse(file_get_contents($source_app . '/app/config/config.yml'));
+        
         $sysInfo=(new App())->sysInformation();
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'sysinfo'=>$sysInfo,
             'datas'=>$sysInfo->data,
+            'version'=>$file['twig']['globals']['version'],
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
