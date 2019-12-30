093f279c60c447a53eea179421f7b7f5eae10337
Sebastiõ Dias Lutonda <lutonda@gmail.com>
2019-12-30 13:51:12 +0000

    config file

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -36,7 +36,7 @@ class AppKernel extends Kernel
     
     public function __construct($environment, $debug)
     {
-        date_default_timezone_set( 'Europe/Paris' );
+        date_default_timezone_set( 'UTC' );
         parent::__construct($environment, $debug);
     }
     public function getRootDir()
