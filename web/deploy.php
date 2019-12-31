67cf0185cd49c4f3453e2c8d43712320388482b8
Sebastiõ Dias Lutonda <lutonda@gmail.com>
2019-12-31 10:35:26 +0000

    conflit fix

--- web/deploy.php
+++ web/deploy.php
@@ -16,7 +16,7 @@ $commands = array(
     'echo $PWD',
     'whoami',
     'git checkout -- .', 
-    'git pull origin master',
+    'git pull',
     'git fetch --tags',
     'git status',
     'git submodule sync',
@@ -32,23 +32,10 @@ $output = '';
 foreach($commands AS $command){
     $tmp = shell_exec($command);
     
-    $output .= "<span style=\"color: #6BE234;\">\$</span><span style=\"color: #729FCF;\">{$command}\n</span><br />";
-    $output .= htmlentities(trim($tmp)) . "\n<br /><br />";
+    //$output []= "<span style=\"color: #6BE234;\">\$</span><span style=\"color: #729FCF;\">{$command}\n</span><br />";
+    $output []= htmlentities(trim($tmp)) . "\n<br /><br />";
 }
 ?>
 
-<!DOCTYPE HTML>
-<html lang="en-US">
-<head>
-    <meta charset="UTF-8">
-    <title>GIT DEPLOYMENT SCRIPT</title>
-</head>
-<body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
-<div style="width:700px">
-    <div style="float:left;width:350px;">
-    <p style="color:white;">Git Deployment Script</p>
-    <?php echo $output; ?>
-    </div>
-</div>
-</body>
-</html>
+    <?php print_r(json_encode( $output); ?>
+    
