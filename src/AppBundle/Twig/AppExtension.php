8b369b001f7b33e747c2f0340273228c177d7435
Sebastiõ Dias Lutonda <lutonda@gmail.com>
2020-06-09 20:43:36 +0100

    remove demo invoice tag

--- src/AppBundle/Twig/AppExtension.php
+++ src/AppBundle/Twig/AppExtension.php
@@ -4,6 +4,7 @@
 namespace AppBundle\Twig;
 
 use AppBundle\Application\Application as App;
+use AppBundle\Entity\Payment;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
 
@@ -13,9 +14,31 @@ class AppExtension extends AbstractExtension
     {
         return [
             new TwigFilter('app_version', [$this, 'appVersion']),
+            new TwigFilter('payment_status', [$this, 'paymentStatus']),
+            new TwigFilter('less_than', [$this, 'lessThan']),
         ];
     }
 
+    public function lessThan($date){
+        return $date<new \DateTime();
+    }
+    public function paymentStatus(Payment $payment,$i=true){
+
+        $icon=['check','times','calendar'];
+        $color=['success','danger','default'];
+        $now=new \DateTime();
+        if($payment->getDueDate() > $now ){
+            $final=2;
+        }
+        else if($payment->getDueDate() < $now){
+            $final=1;
+        }
+        else{
+            $final=0;
+        }
+
+        return $i ? $icon[$final] : $color[$final];
+    }
     public function appVersion($name)
     {
 
