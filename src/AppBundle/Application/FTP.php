47afb6dcce4b26115add1f9112b491808bbe8106
Sebastiõ Dias Lutonda <lutonda@gmail.com>
2020-06-10 21:59:14 +0100

    ftp and file creationfrom server by cpanl and server stash

--- src/AppBundle/Application/FTP.php
+++ src/AppBundle/Application/FTP.php
@@ -12,19 +12,28 @@ class FTP extends Application implements IApplication{
     }
 
 
-    public function create($name){
+    public function create($name)
+    {
+
+        $path = $this->path;
+
+        if (strpos($name, 'free'))
+        {
+            $name = str_replace('.free', '', $name);
+            $path.='../free/';
+        }
 
         try {
 
             var_dump('Entering path: /home/novanet/apps/ <hr/>');
-            chdir("/home/novanet/apps/");
+            chdir($path);
 
-            var_dump('Cloning repo to path: /home/novanet/apps/'.$name.'<hr/>');
+            var_dump('Cloning repo to path: '.$path.'<hr/>');
             $outputs = trim(exec('git clone git@gitlab.com:lutonda/nova_erp.git '.$name));
             var_dump($outputs.'<hr/>');
 
-            var_dump('Entering path: /home/novanet/apps/'.$name.'<hr/>');
-            chdir("/home/novanet/apps/".$name);
+            var_dump('Entering path: '.$path.$name.'<hr/>');
+            chdir($path.$name);
 
             var_dump('checkout $(git describe --tags)<hr/>');
             var_dump('checkout $(git tag | sort -n | tail -1)<hr/>');
@@ -37,15 +46,21 @@ class FTP extends Application implements IApplication{
         }
         print_r('<hr>');
         print_r($this->path .  $name . '/web');
-        return $this->path .  $name . '/web';
+        return $path .  $name . '/web';
     }
 
     public function createStach($name){
+        $path = $this->path;
 
+        if (strpos($name, 'free'))
+        {
+            $name = str_replace('.free', '', $name);
+            $path.='../free/';
+        }
         try {
 
             var_dump('Instaling vendors<hr/>');
-            $outputs=exec('cp -r '.$this->path . 'demo/vendor/ '.$this->path .  $name . '/vendor/');
+            $outputs=exec('cp -r '.$this->path . 'demo/vendor/ '.$path .  $name . '/vendor/');
             var_dump($outputs.'<hr/>');
             var_dump('Installing packages<hr/>');
             $outputs = trim(exec('composer install'));
