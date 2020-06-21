269d95693a5c71d53d11cd3f4190ffd798c54af4
Sebastiõ Dias Lutonda <lutonda@gmail.com>
2020-06-21 07:05:39 +0100

    plan and payment set

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -3,8 +3,9 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Application\Application as App;
+use AppBundle\Entity\Plan;
+use FOS\RestBundle\Controller\Annotations as Rest;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -41,6 +42,17 @@ class DefaultController extends Controller
      */
     public function sysUpgradeAction(){
         return new Response(json_encode(App::upgrade()),200,['Content-Type'=>' application/json']);
+
+    }
+
+
+    /**
+     * @Rest\Get("/Api/V1/plans")
+     */
+    public function apiPlansAction(){
+
+        $final=$this->getDoctrine()->getRepository(Plan::class)->findAll();
+        return $final;
 
     }
 }
