4ad40de4e50e58d492b53be78d1c05e147f20605
Sebastiõ Dias Lutonda <lutonda@gmail.com>
2020-08-10 20:00:17 +0100

    plan and payment set

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Mopa\Bundle\BootstrapBundle\MopaBootstrapBundle(),
             new AppBundle\AppBundle(),
+            new ApiBundle\ApiBundle(),
 
             new FOS\RestBundle\FOSRestBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle()
